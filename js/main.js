5df0dc519a79938894bf6ba482c9c4a05d45ed27
Duy Anh <huynhanh.mtu@gmail.com>
2021-12-13 16:09:05 +0700

    Update

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
+// Scroll to top on page reload
+window.onload = function () {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+  // window.scrollTo(0, 0); => instant scrolling
+}
+
 // Progress bar
-function move(maxPercent,barID,spanID) {
+function runProgressBar(maxPercent, barID, spanID) {
   var percent = 0;
   if (percent == 0) {
     percent = 1;
@@ -16,6 +22,36 @@ function move(maxPercent,barID,spanID) {
     }
   }
 }
-move(73,"progressBar1","span1");
-move(48,"progressBar2","span2");
-move(65,"progressBar3","span3");
+// Test tính năng chạy animation progress bar khi scrool đến vị trí (fail)
+var doFunction = true;
+window.addEventListener("scroll", function () {
+  var topScreen = window.pageYOffset;
+  var bottomScreen = topScreen + window.innerHeight;
+  var topDIV = document.getElementById("progress__bar").offsetTop;
+  var bottomDIV = topDIV + document.getElementById("progress__bar").scrollHeight;
+  if (doFunction && topDIV <= bottomScreen <= bottomDIV) {
+    runProgressBar(73, "progressBar1", "span1");
+    runProgressBar(48, "progressBar2", "span2");
+    runProgressBar(65, "progressBar3", "span3");
+    doFunction = false;
+  }
+  console.log(doFunction);
+  console.log(topDIV, bottomScreen ,bottomDIV);
+  console.log(topDIV <= bottomScreen <= bottomDIV); //?
+});
+
+// Header on scroll
+window.onscroll = function () {
+  if (
+    document.body.scrollTop > 50 ||
+    document.documentElement.scrollTop > 50
+  ) {
+    document.getElementById("header").style.cssText =
+      "height:60px; box-shadow: 0 0 2px 0 #38455433; opacity: 0.95;";
+    document.getElementById("header__img").style.cssText = "width: 132px";
+  } else {
+    document.getElementById("header").style.cssText =
+      "height: 100px; box-shadow: none; opacity: 1";
+    document.getElementById("header__img").style.cssText = "width: 164px";
+  }
+}
